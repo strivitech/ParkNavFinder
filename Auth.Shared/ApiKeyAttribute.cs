f87c0c77095dc3ae389228c9f564fe2c0b758c30
strivitech <strivitech@gmail.com>
2023-12-20 16:52:44 +0200

    Add Auth.Shared project containing shared authentication and configurations data

---
+++ Auth.Shared/ApiKeyAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Shared;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute(string apiKeyName) : Attribute, IAuthorizationFilter
+{
+    private readonly string _apiKeyName = apiKeyName;
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        string? apiKey = context.HttpContext.Request.Headers[ApiKeyConstants.HeaderPrefix + _apiKeyName];
+        
+        var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+        
+        if (!IsApiKeyValid(configuration, apiKey))
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+
+    private static bool IsApiKeyValid(IConfiguration configuration, string? apiKey)
+    {
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {   
+            return false;
+        }
+
+        var apiKeyFromConfig = configuration
+            .GetValue<string>($"{ApiKeyConstants.ConfigSection}:{ApiKeyConstants.ParkingOfferingService}");
+        
+        return apiKey == apiKeyFromConfig;
+    }
+}
---
+++ Auth.Shared/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Shared;
+
+public static class Roles
+{
+    public const string User = nameof(User);
+    public const string Provider = nameof(Provider);
+    public const string Admin = nameof(Admin);
+}
