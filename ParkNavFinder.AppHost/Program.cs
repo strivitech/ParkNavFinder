e115568f2bbc83a86b900df34d128c96307aa4a8
strivitech <strivitech@gmail.com>
2023-12-21 16:41:27 +0200

    Remove custom auth server

--- ParkNavFinder.AppHost/Program.cs
+++ ParkNavFinder.AppHost/Program.cs
@@ -1,9 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var userRedis = builder.AddRedisContainer("userredis");
-builder.AddProject<Projects.UserService>("userservice")
-    .WithReference(userRedis);
-
 builder.AddProject<Projects.UserWsHandler>("userwshandler");
 
 builder.Build().Run();
