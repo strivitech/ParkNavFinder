4c0c8c66faf00c5680df0990d9db19457cbfe6d4
strivitech <strivitech@gmail.com>
2023-12-23 14:17:41 +0200

    Add UsersHub that links user to host using WebSocket connection

---
+++ UserWsHandler/Services/IWsManagerService.cs
@@ -0,0 +1,7 @@
+﻿namespace UserWsHandler.Services;
+
+public interface IWsManagerService
+{
+    Task SendUserConnectedMessage(string userId);
+    Task SendUserDisconnectedMessage(string userId);
+}
