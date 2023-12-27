85146969478df1608d4db65528ab3dcc97585044
strivitech <strivitech@gmail.com>
2023-12-27 14:54:03 +0200

    Create LocationService Project. Provide endpoint & services to post user locations to Kafka cluster for further processing

---
+++ LocationService/Common/ModelValidationException.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+
+namespace LocationService.Common;
+
+/// <summary>
+/// An exception if a model isn't valid.
+/// </summary>
+[Serializable]
+public class ModelValidationException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="ModelValidationException"/>.
+    /// </summary>
+    public ModelValidationException()
+        : base("One or more validation failures have occurred.")
+    {
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="ModelValidationException"/>.
+    /// </summary>
+    /// <param name="failures">Validation failures.</param>
+    public ModelValidationException(IEnumerable<ValidationFailure> failures)
+        : this()
+    {
+        Errors = failures
+            .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+            .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+    }
+
+    /// <summary>
+    /// Contains validation errors.
+    /// </summary>
+    public IDictionary<string, string[]> Errors { get; }
+}
---
+++ LocationService/Services/UserLocationService.cs
@@ -0,0 +1,34 @@
+﻿using KafkaFlow;
+using KafkaFlow.Producers;
+using LocationService.Common.Configuration;
+using LocationService.Models;
+using LocationService.Requests;
+
+namespace LocationService.Services;
+
+public class UserLocationService(
+    IProducerAccessor producerAccessor,
+    ILogger<UserLocationService> logger,
+    IModelValidationService modelValidationService)
+    : IUserLocationService
+{
+    private readonly IMessageProducer _messageProducer = producerAccessor.GetProducer(KafkaConstants.ProducerName);
+    private readonly ILogger<UserLocationService> _logger = logger;
+    private readonly IModelValidationService _modelValidationService = modelValidationService;
+    
+    public async Task PostNewLocation(PostUserLocationRequest postUserLocationRequest)
+    {
+        _modelValidationService.ThrowIfNotValid(postUserLocationRequest);
+
+        _logger.LogDebug(
+            "Posting new location for user {UserId}, Latitude: {Latitude}, Longitude: {Longitude}, DateTime: {Timestamp}",
+            postUserLocationRequest.UserId, postUserLocationRequest.Latitude, postUserLocationRequest.Longitude,
+            postUserLocationRequest.Timestamp);
+
+        var message = UserLocationMessage.FromPostUserLocationRequest(postUserLocationRequest);
+
+        _ = await _messageProducer.ProduceAsync(KafkaConstants.UserLocationTopic, message.UserId, message);
+
+        _logger.LogDebug("New location posted for user {UserId}", postUserLocationRequest.UserId);
+    }
+}
