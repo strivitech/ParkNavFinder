a502d41ed002e361916d83ec78f888bf5cf3dad7
strivitech <111649007+strivitech@users.noreply.github.com>
2024-01-04 18:05:59 +0200

    Manually add ports & passwords, so they are static. (#12)

--- ParkNavFinder.AppHost/Program.cs
+++ ParkNavFinder.AppHost/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var userWsHandler = builder.AddProject<Projects.UserWsHandler>("userwshandler");
 
-var webSocketManagerRedis = builder.AddRedisContainer("webSocketManagerRedis");
+var webSocketManagerRedis = builder
+    .AddRedisContainer(
+        name: "webSocketManagerRedis",
+        port: builder.Configuration.GetValue<int>("webSocketManagerRedis:port"));
 var webSocketManager = builder.AddProject<Projects.WebSocketManager>("websocketmanager");
 
 var locationService = builder.AddProject<Projects.LocationService>("locationservice");
@@ -19,14 +24,20 @@ builder.AddProject<Projects.Yarp_ApiGateway>("yarpapigateway")
     .WithReference(userWsHandler)
     .WithLaunchProfile("https");
 
-var userActiveGeoIndexRedis = builder.AddRedisContainer("userActiveGeoIndexRedis");
+var userActiveGeoIndexRedis = builder
+    .AddRedisContainer(
+        name: "userActiveGeoIndexRedis",
+        port: builder.Configuration.GetValue<int>("userActiveGeoIndexRedis:port"));
 
 builder.AddProject<Projects.UserActiveGeoIndexService>("useractivegeoindexservice")
     .WithReference(userActiveGeoIndexRedis)
     .WithReference(mapService);
 
 var parkingManagementServicePostgres = builder
-    .AddPostgresContainer("parkingManagementPostgres", port: 8001, password: "admin")
+    .AddPostgresContainer(
+        name: "parkingManagementPostgres", 
+        port: builder.Configuration.GetValue<int>("parkingsdb:port"),
+        password: builder.Configuration.GetValue<string>("parkingsdb:password"))
     .AddDatabase("parkingsdb");
 
 builder.AddProject<Projects.ParkingManagementService>("parkingmanagementservice")
