8c88a21ef1d5020485548715e03a860a51548c27
strivitech <strivitech@gmail.com>
2024-01-04 17:45:51 +0200

    Update code & descriptions of service Errors

--- ParkingManagementService/Common/Errors.cs
+++ ParkingManagementService/Common/Errors.cs
@@ -6,20 +6,18 @@ public static class Errors
 {
     public static class Parking
     {
-        public static Error NotFound(Guid id) => Error.NotFound("Parking with id {0} not found.", id.ToString());
-        public static Error AddFailed(Guid id) => Error.Failure("Failed to add parking with id {0}.", id.ToString());
+        public static Error NotFound(Guid id) => Error.NotFound("Parking.NotFound", $"Parking with id {id.ToString()} not found.");
+        public static Error AddFailed(Guid id) => Error.Failure("Parking.AddFailed", $"Failed to add parking with id {id.ToString()}.");
         
-        public static Error UpdateFailed(Guid id) => Error.Failure("Failed to update parking with id {0}.", id.ToString());
+        public static Error UpdateFailed(Guid id) => Error.Failure("Parking.UpdateFailed", $"Failed to update parking with id {id.ToString()}.");
         
-        public static Error DeleteFailed(Guid id) => Error.Failure("Failed to delete parking with id {0}.", id.ToString());
-        
-        public static Error LocationCannotBeChanged() => Error.Failure("Parking location cannot be changed.");
+        public static Error DeleteFailed(Guid id) => Error.Failure("Parking.DeleteFailed", $"Failed to delete parking with id {id.ToString()}.");
 
-        public static Error NotOwnedByCurrentUser(Guid id) => Error.Unauthorized("Parking with id {0} is not owned by current user.", id.ToString());
+        public static Error NotOwnedByCurrentUser(Guid id) => Error.Unauthorized("Parking.NotOwnedByCurrentUser", $"Parking with id {id.ToString()} is not owned by current user.");
     }
     
     public static class EventPublisher
     {
-        public static Error PublishFailed(string eventName) => Error.Unexpected("Failed to publish event: {0}.", eventName);
+        public static Error PublishFailed(string eventName) => Error.Unexpected("EventPublisher.PublishFailed", $"Failed to publish event: {eventName}.");
     }
 }
