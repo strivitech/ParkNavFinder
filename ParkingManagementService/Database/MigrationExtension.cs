528468fec32ea73f9cc91e77b17e710a19765a22
strivitech <strivitech@gmail.com>
2024-01-04 17:49:26 +0200

    Use development easy create & delete db operations with EF Core API

---
+++ ParkingManagementService/Database/MigrationExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingManagementService.Database;
+
+public static class MigrationExtension
+{
+    public static void EnsureDbCreated(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ParkingDbContext>();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+}
--- ParkingManagementService/Database/ParkingDbContext.cs
+++ ParkingManagementService/Database/ParkingDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using ParkingManagementService.Models;
 
 namespace ParkingManagementService.Database;
 
 public class ParkingDbContext(DbContextOptions<ParkingDbContext> options) : DbContext(options)
 {
-    public DbSet<Parking> ParkingSet => Set<Parking>();         
+    public DbSet<Parking> ParkingSet => Set<Parking>();    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
