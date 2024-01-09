9304d1ddaf0ae1adb8cb984547acfae0cc6858f0
strivitech <strivitech@gmail.com>
2024-01-09 16:53:34 +0200

    Remove seeder

--- ParkingStateService/Database/ParkingStateDbContext.cs
+++ ParkingStateService/Database/ParkingStateDbContext.cs
@@ -13,20 +13,7 @@ public class ParkingStateDbContext(DbContextOptions<ParkingStateDbContext> optio
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        
-        modelBuilder.Entity<ActiveIndex>().HasData(SeedActiveIndices());
 
         base.OnModelCreating(modelBuilder);
-    }
-    
-    private static ActiveIndex[] SeedActiveIndices()
-    {
-        var activeIndices = new List<ActiveIndex>();
-        for (var i = 1; i <= 250; i++)
-        {
-            activeIndices.Add(new ActiveIndex { Index = $"H3Index_{i}", LastUpdatedUtc = DateTime.UtcNow.AddMinutes(-7) });
-        }
-
-        return activeIndices.ToArray();
     }
 }
