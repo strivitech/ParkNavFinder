9224e0f48711863b0bfb1edfbdf2eed9e48ad1f5
strivitech <strivitech@gmail.com>
2024-02-17 13:55:03 +0200

    Add tests for User.NotificationService

---
+++ Tests/User.NotificationService.Tests/WebsocketManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using FluentAssertions;
+using NSubstitute;
+using Shared;
+using User.NotificationService.Services;
+
+namespace User.NotificationService.Tests;
+
+public class WebsocketManagerTests
+{
+    private readonly MockHttpMessageHandler _mockHttpMessageHandler;
+    private readonly WebsocketManager _service;
+
+    public WebsocketManagerTests()
+    {
+        _mockHttpMessageHandler = Substitute.ForPartsOf<MockHttpMessageHandler>();
+        var httpClient = new HttpClient(_mockHttpMessageHandler)
+        {
+            BaseAddress = new Uri("http://example.com/")
+        };
+
+        _service = new WebsocketManager(httpClient);
+    }
+
+    [Fact]
+    public async Task GetHandlersAsync_WhenSuccessful_ReturnsListOfUserHandlerDescriptions()
+    {
+        // Arrange
+        var userIds = new List<string> { "user1", "user2" };
+        var idsHandlers = new Dictionary<string, string?>
+        {
+            {"user1", "handler1"},
+            {"user2", "handler2"}
+        };
+        var mockResponse = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(JsonSerializer.Serialize(idsHandlers), Encoding.UTF8, "application/json")
+        };
+        _mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>())
+            .Returns(mockResponse);
+
+        // Act
+        var result = await _service.GetHandlersAsync(userIds);
+
+        // Assert
+        result.Should().HaveCount(idsHandlers.Count);
+        result.Should().Contain(items => items.UserId == "user1" && items.Handler == "handler1");
+        result.Should().Contain(items => items.UserId == "user2" && items.Handler == "handler2");
+    }
+
+    [Fact]
+    public async Task GetHandlersAsync_WhenUnsuccessfulHttpResponse_ThrowsHttpRequestException()
+    {   
+        // Arrange  
+        var userIds = new List<string> { "user1", "user2" };
+        var mockResponse = new HttpResponseMessage(HttpStatusCode.BadRequest);
+        _mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>())
+            .Returns(mockResponse);
+
+        // Act & Assert
+        Func<Task> act = async () => await _service.GetHandlersAsync(userIds);
+        await act.Should().ThrowAsync<HttpRequestException>();
+    }
+}
--- User.NotificationService/Services/WebsocketManager.cs
+++ User.NotificationService/Services/WebsocketManager.cs
@@ -1,9 +1,10 @@
-﻿using System.Text.Json;
+﻿using System.Runtime.Serialization;
+using System.Text.Json;
 using User.NotificationService.Contracts;
 
 namespace User.NotificationService.Services;
 
-internal class WebsocketManager(HttpClient httpClient) : IWebsocketManager
+public class WebsocketManager(HttpClient httpClient) : IWebsocketManager
 {
     private readonly HttpClient _httpClient = httpClient;
 
@@ -15,8 +16,9 @@ internal class WebsocketManager(HttpClient httpClient) : IWebsocketManager
         response.EnsureSuccessStatusCode();
 
         var content = await response.Content.ReadAsStringAsync();
-        var idsHandlers = JsonSerializer.Deserialize<Dictionary<string, string?>>(content) ?? [];
-        return idsHandlers.Select(x => 
+        var idsHandlers = JsonSerializer.Deserialize<Dictionary<string, string?>>(content) ??
+                          throw new SerializationException("Failed to deserialize response");
+        return idsHandlers.Select(x =>
                 new UserHandlerDescription { UserId = x.Key, Handler = x.Value })
             .ToList();
     }
