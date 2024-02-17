606b684a1788369e22cdf7671fa0c7a80b5af17a
strivitech <strivitech@gmail.com>
2024-02-17 13:55:24 +0200

    Add additional test for GeoIndexServiceTests

--- Tests/User.LocationService.Tests/GeoIndexServiceTests.cs
+++ Tests/User.LocationService.Tests/GeoIndexServiceTests.cs
@@ -46,6 +46,22 @@ public class GeoIndexServiceTests
     }
 
     [Fact]
+    public async Task GetGeoIndexAsync_WhenUnsuccessfulHttpResponse_ThrowsHttpRequestException()
+    {
+        // Arrange
+        const double lat = 40.7128;
+        const double lon = 28.0060;
+        const int resolution = 10;
+        var mockResponse = new HttpResponseMessage(HttpStatusCode.BadRequest);
+        _mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>())
+            .Returns(mockResponse);
+
+        // Act & Assert
+        Func<Task> act = async () => await _service.GetGeoIndexAsync(lat, lon, resolution);
+        await act.Should().ThrowAsync<HttpRequestException>();
+    }
+    
+    [Fact]
     public async Task GetGeoIndexAsync_WhenResponseContentIsEmpty_ThrowsJsonException()
     {
         // Arrange
