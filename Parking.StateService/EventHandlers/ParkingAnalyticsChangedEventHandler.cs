d25426879f7a1cabbf73b3557d11057ab6be5561
strivitech <111649007+strivitech@users.noreply.github.com>
2024-04-10 22:45:14 +0300

    Add kafka consumer for ParkingAnalyticsChangedEvent (#31)

---
+++ Parking.StateService/EventHandlers/ParkingAnalyticsChangedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using EFCore.BulkExtensions;
+using Kafka.Events.Contracts.Parking.State;
+using KafkaFlow;
+using Microsoft.EntityFrameworkCore;
+using Parking.StateService.Database;
+using Parking.StateService.Domain;
+
+namespace Parking.StateService.EventHandlers;
+
+public class ParkingAnalyticsChangedEventHandler : IMessageHandler<ParkingAnalyticsChangedEvent>
+{
+    public async Task Handle(IMessageContext context, ParkingAnalyticsChangedEvent message)
+    {
+        var logger = context.DependencyResolver.Resolve<ILogger<ParkingAnalyticsChangedEventHandler>>();
+        var dbContextFactory = context.DependencyResolver.Resolve<IDbContextFactory<ParkingStateDbContext>>();
+        logger.LogDebug("ParkingAnalyticsChangedEvent received");
+
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+
+        var parkingStatesToUpdate = message.ParkingStateInfos
+            .Select(psi => new ParkingState
+            {
+                ParkingId = psi.ParkingId,
+                TotalObservers = psi.TotalObservers,
+                Probability = psi.Probability,
+                LastCalculatedUtc = psi.LastCalculatedUtc
+            }).ToList();
+
+        await dbContext.BulkUpdateAsync(parkingStatesToUpdate,
+            new BulkConfig { PropertiesToExclude = new List<string> { nameof(ParkingState.GeoIndex) } });
+
+        logger.LogDebug("Parking states updated");
+    }
+}
